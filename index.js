155f2055cc855182a8fc53ba4ea7760b9f3f84fe
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-14 17:34:35 -0500

    stdin and stdout example

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("HAPPY WEDNESDAY");
+// prompt the user for a string
+process.stdout.write("What is for dinner?");
+
+// listen for the user input
+process.stdin.once("data", (data) => {
+    console.log("You said: " + data.toString().trim());
+    process.exit(0);
+})
